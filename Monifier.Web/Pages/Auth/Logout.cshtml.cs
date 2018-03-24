0ea522db3dac9c2306c29e07604d64debf974493
Ivan-Proskurin <ivan.proskurin@gmail.com>
2018-03-24 18:23:19 +0300

    logout link

---
+++ Monifier.Web/Pages/Auth/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Monifier.Web.Auth;
+
+namespace Monifier.Web.Pages.Auth
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await HttpContext.SignOutAsync(AuthConsts.AuthenticationScheme);
+            return RedirectToPage("/Auth/Login");
+        }
+    }
+}
